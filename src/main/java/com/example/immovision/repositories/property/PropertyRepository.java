ac75d2fbd9ebcd090dfc760cd35bfcc327e0a2aa
wahaj <wahaj.ahmed45@gmail.com>
2025-01-19 16:07:06 +0100

    corrections round 4

--- src/main/java/com/example/immovision/repositories/property/PropertyRepository.java
+++ src/main/java/com/example/immovision/repositories/property/PropertyRepository.java
@@ -39,7 +39,7 @@ public interface PropertyRepository extends JpaRepository<Property, UUID>, JpaSp
     @Query("SELECT p FROM Property p WHERE " +
             "(:priceMin IS NULL OR p.price >= :priceMin) " +
             "AND (:priceMax IS NULL OR p.price <= :priceMax) " +
-            "AND (:rooms IS NULL OR p.roomCounts = :rooms) " +
+            "AND (:rooms IS NULL OR p.bedroom = :rooms) " +
             "AND (:bathrooms IS NULL OR p.bathroom = :bathrooms) " +
             "AND (:city IS NULL OR p.city = :city)")
     List<Property> findFilteredProperties(
